1a4af946c818c07fcf5d37d9b4682a0832577230
Abril Zacarias <abrilzacarais6@gmail.com>
2023-10-11 18:27:55 -0300

    1

--- firebaseConfig.py
+++ firebaseConfig.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 #carga del certificado del proyecto
-cred = credentials.Certificate("credentials/crud-clientes-8acf7-firebase-adminsdk-w8cuy-2f0db1323c.json")
+cred = credentials.Certificate("credentials/crud-clientes-8acf7-firebase-adminsdk-w8cuy-38c92b009a.json")
 
 #referencia a la base de datos en tiempo real
 firebase_admin.initialize_app(cred,{'databaseURL':'https://crud-clientes-8acf7-default-rtdb.firebaseio.com/'})
